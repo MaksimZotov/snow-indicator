d4741896aec6802e31ffa8be5c8c0dda7f2846c6
MaksimZotov <zotovmaksim1254@gmail.com>
2022-01-19 12:55:20 +0300

    animations

--- lib/presenter/screens/city_info_screen.dart
+++ lib/presenter/screens/city_info_screen.dart
@@ -11,9 +11,13 @@ class CityInfoWidget extends StatefulWidget {
   State<StatefulWidget> createState() => CityInfoState(title);
 }
 
-class CityInfoState extends State<CityInfoWidget> {
+class CityInfoState extends State<CityInfoWidget>
+    with SingleTickerProviderStateMixin {
   File? bg;
   final String title;
+  late AnimationController _controller;
+
+  CityInfoState(this.title);
 
   Future _showChoseBackgroundDialog(BuildContext ctx) async {
     final path = await showDialog(
@@ -29,29 +33,100 @@ class CityInfoState extends State<CityInfoWidget> {
     setState(() {});
   }
 
-  CityInfoState(this.title);
+  @override
+  void initState() {
+    _controller = AnimationController(
+      duration: Duration(milliseconds: 1000),
+      vsync: this,
+    );
+    _controller.repeat();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext ctx) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.add_photo_alternate),
-            onPressed: () => _showChoseBackgroundDialog(ctx),
-          ),
-        ],
-      ),
-      body: bg != null
-          ? Image.file(
-              bg!,
-              fit: BoxFit.cover,
-              height: double.infinity,
-              width: double.infinity,
-              alignment: Alignment.center,
-            )
-          : null,
-    );
+        appBar: AppBar(
+          title: Text(title),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.add_photo_alternate),
+              onPressed: () => _showChoseBackgroundDialog(ctx),
+            ),
+          ],
+        ),
+        body: Stack(
+          fit: StackFit.loose,
+          children: [
+            Container(
+              child: bg != null
+                  ? Image.file(
+                      bg!,
+                      fit: BoxFit.cover,
+                      height: double.infinity,
+                      width: double.infinity,
+                      alignment: Alignment.center,
+                    )
+                  : null,
+            ),
+            Align(
+              child: Text(
+                "24",
+                style: TextStyle(
+                    fontSize: 54,
+                    color: Colors.lightBlueAccent,
+                    background: Paint()
+                      ..color = Colors.blue
+                      ..style = PaintingStyle.stroke),
+              ),
+            ),
+            Positioned(
+              top: 25,
+              left: 25,
+              child: RotationTransition(
+                turns: Tween(begin: 0.0, end: 1.0).animate(_controller),
+                child: const Image(
+                  image: AssetImage('assets/images/snowflake.png'),
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 25,
+              left: 25,
+              child: RotationTransition(
+                turns: Tween(begin: 0.0, end: 1.0).animate(_controller),
+                child: const Image(
+                  image: AssetImage('assets/images/snowflake.png'),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 25,
+              right: 25,
+              child: RotationTransition(
+                turns: Tween(begin: 0.0, end: 1.0).animate(_controller),
+                child: const Image(
+                  image: AssetImage('assets/images/snowflake.png'),
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 25,
+              right: 25,
+              child: RotationTransition(
+                turns: Tween(begin: 0.0, end: 1.0).animate(_controller),
+                child: const Image(
+                  image: AssetImage('assets/images/snowflake.png'),
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
